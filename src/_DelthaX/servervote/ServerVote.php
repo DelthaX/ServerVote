de410d3c3260cee1eb735a035fe99cbb30a6fd58
DelthaX <cao23750@gmail.com>
2015-10-25 13:42:25 +0900

    Create ServerVote.php

---
+++ src/_DelthaX/servervote/ServerVote.php
@@ -0,0 +1,17 @@
+<?php
+namespace _DelthaX\servervote;
+use pocketmine\plugin\PluginBase;
+class ServerVote extends PluginBase{
+	private static $api = null;
+	private $resources = ["settings.yml", "list.yml"];
+	public function onLoad(){
+		if(self::$api == null) self::$api = $this;
+		$this->loadResources();
+	}
+	private function loadResources(){
+		foreach($this->resources as $file){
+			$this->saveResources($file, false);
+		}
+	}
+}
+?>
